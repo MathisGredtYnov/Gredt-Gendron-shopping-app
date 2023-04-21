e2b0a6274ffc02ea9540af9a67f9b66d64688585
MathisGredtYnov <mathis.gredt@ynov.com>
2023-04-21 11:55:35 +0200

    avancement API et affichage des données

--- BACKEND/script.js
+++ BACKEND/script.js
@@ -9,6 +9,26 @@ app.use(cors());{
     origin: '*'
 }
 
+//routes//
+app.get('/pantalons', (req, res) => {
+    const pantalons = data.pantalons;
+    res.json(pantalons);
+});
+
+app.get('/pantalons/:id', (req, res) => {
+    const id = req.params.id;
+    const pantalons = data.pantalons;
+    const pantalon = pantalons.find(pantalon => pantalon.id == id);
+    if (!pantalons){
+        res.status(404).send('Pantalon not found');
+    } else {
+        res.status(200).json(pantalon);
+        message = "Pantalon found";
+        pantalons = pantalon;
+    }
+});
+
+
 //lancement du serveur//
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}"`);
---
+++ FRONTEND/JS/page.js
@@ -0,0 +1,70 @@
+const port = 3000;
+const ip_add = "localhost";
+const url = `http://${ip_add}:${port}`;
+const container = document.querySelector("second_content");
+const pickers = document.querySelectorAll("picker");
+
+let pantalons
+let filteredPantalons
+// Fetching data from the server
+function GetPantalons() {
+    fetch(url + "/pantalons")
+        .then((response) => response.json())
+        .then((data) => {
+            pantalons = data;
+            filteredPantalons = pantalons;
+            console.log(data);
+            DisplayPantalons();
+        })
+        .catch((err) => console.log(err))
+}
+
+// Displaying data from the server
+function DisplayPantalons() {
+    container.innerHTML = "";
+    filteredPantalons.forEach((pantalon) => {
+        let pantalonCTN = document.createElement("div");
+        pantalonCTN.classList.add("pantalon_item");
+        container.innerHTML += `
+            <img src="${pantalon.img_1}" alt="${pantalon.name}" />
+            <div class="card-body">
+                <h3>${pantalon.name}</h3>
+                <p>${pantalon.price}€</p>
+                <button onclick="AddToCart(${pantalon.id})">Ajouter au panier</button>
+            </div>
+        `;
+        container.appendChild(pantalonCTN);
+    });
+}
+
+// Filtering data from the server
+pickers.forEach(picker => {    
+    picker.addEventlistener("click", SelectItem);
+});
+
+function SelectItem(e){
+    let picker = e.target;
+    let color = e.target.classList[2];
+    pickers.forEach((e) => {
+        e.classList.remove("selected");
+    });
+    picker.classList.add("selected");
+    console.log(color);
+    filterByColor(color);
+}
+
+function filterByColor(color){
+    if (color === "all"){
+        filteredPantalons = pantalons;
+        DisplayPantalons();
+    } else {
+        filteredPantalons = pantalons.filter((pantalon) => pantalon.color === color);
+        container.innerHTML = "";
+        if (filteredPantalons.length <= 0){
+            container.innerHTML = `
+                <h3>Aucun pantalon trouvé</h3>`;   
+        } else {
+            DisplayPantalons();
+        }
+    }
+}
