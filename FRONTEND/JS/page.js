f2a0b9642dc464039896fc298f5c82e64342e7fd
MathisGredtYnov <mathis.gredt@ynov.com>
2023-04-22 11:35:05 +0200

    débug donnée api

--- FRONTEND/JS/page.js
+++ FRONTEND/JS/page.js
@@ -1,11 +1,12 @@
 const port = 3000;
 const ip_add = "localhost";
 const url = `http://${ip_add}:${port}`;
-const container = document.querySelector("second_content");
-const pickers = document.querySelectorAll("picker");
+const container = document.querySelector(".second_content");
+const pickers = document.querySelectorAll(".picker");
 
 let pantalons
 let filteredPantalons
+
 // Fetching data from the server
 function GetPantalons() {
     fetch(url + "/pantalons")
@@ -15,14 +16,13 @@ function GetPantalons() {
             filteredPantalons = pantalons;
             console.log(data);
             DisplayPantalons();
-        })
-        .catch((err) => console.log(err))
+        });
 }
 
 // Displaying data from the server
 function DisplayPantalons() {
     container.innerHTML = "";
-    filteredPantalons.forEach((pantalon) => {
+    filteredPantalons.forEach(pantalon => {
         let pantalonCTN = document.createElement("div");
         pantalonCTN.classList.add("pantalon_item");
         container.innerHTML += `
@@ -39,7 +39,7 @@ function DisplayPantalons() {
 
 // Filtering data from the server
 pickers.forEach(picker => {    
-    picker.addEventlistener("click", SelectItem);
+    picker.addEventListener("click", SelectItem);
 });
 
 function SelectItem(e){
@@ -53,12 +53,13 @@ function SelectItem(e){
     filterByColor(color);
 }
 
+// Filtering data color 
 function filterByColor(color){
     if (color === "all"){
         filteredPantalons = pantalons;
         DisplayPantalons();
     } else {
-        filteredPantalons = pantalons.filter((pantalon) => pantalon.color === color);
+        filteredPantalons = pantalons.filter(pantalon => pantalon.colors === color);
         container.innerHTML = "";
         if (filteredPantalons.length <= 0){
             container.innerHTML = `
